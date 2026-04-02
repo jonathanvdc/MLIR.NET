ad33cb17f902df7889abfbe55bee68708550fb68
jonathanvdc <jonathan.vdc@outlook.com>
2026-04-02 19:45:49 -0400

    Refactor DialectEmitter to utilize DialectFileEmitter and DialectRegistrationEmitter for improved code organization and readability

---
+++ src/MLIR.Generators/Emitters/DialectRegistrationEmitter.cs
@@ -0,0 +1,44 @@
+namespace MLIR.Generators.Emitters;
+
+using System.Text;
+using MLIR.ODS.Model;
+
+internal static class DialectRegistrationEmitter
+{
+    public static void Emit(StringBuilder builder, DialectModel dialect)
+    {
+        var dialectClassName = DialectGeneratorNaming.GetDialectRegistrationClassName(dialect);
+
+        EmitterHelpers.AppendXmlDocComment(builder, dialect.Summary, dialect.Description);
+        builder.AppendLine("public static class " + dialectClassName);
+        builder.AppendLine("{");
+        builder.AppendLine("    public static Dialect Create()");
+        builder.AppendLine("    {");
+        builder.AppendLine("        return Dialect.Create(\"" + dialect.Name + "\", dialect =>");
+        builder.AppendLine("        {");
+
+        foreach (var operation in dialect.Operations)
+        {
+            builder.AppendLine("            dialect.AddOperation(" + DialectGeneratorNaming.GetOperationClassName(operation) + ".OperationDefinition);");
+        }
+
+        foreach (var attribute in dialect.Attributes)
+        {
+            builder.AppendLine("            dialect.AddAttribute(" + DialectGeneratorNaming.GetAttributeClassName(attribute) + ".AttributeDefinition);");
+        }
+
+        foreach (var attributeConstraint in dialect.AttributeConstraints)
+        {
+            builder.AppendLine("            dialect.AddAttributeConstraint(" + DialectGeneratorNaming.GetAttributeConstraintClassName(attributeConstraint) + ".AttributeConstraintDefinition);");
+        }
+
+        foreach (var type in dialect.Types)
+        {
+            builder.AppendLine("            dialect.AddType(" + DialectGeneratorNaming.GetTypeClassName(type) + ".TypeDefinition);");
+        }
+
+        builder.AppendLine("        });");
+        builder.AppendLine("    }");
+        builder.AppendLine("}");
+    }
+}
