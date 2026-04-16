a215a09e2d3cf0890446ce3e957825c30397e4b0
jonathanvdc <jonathan.vdc@outlook.com>
2026-04-16 12:34:29 -0400

    Remove unnecessary using directive in AttributeEmitter to improve code clarity

--- src/MLIR.Generators/Emitters/AttributeEmitter.cs
+++ src/MLIR.Generators/Emitters/AttributeEmitter.cs
@@ -1,7 +1,6 @@
 namespace MLIR.Generators.Emitters;
 
 using System.Text;
-using MLIR.Generators.Emitters;
 using MLIR.Generators.Emitters.AssemblyFormat;
 using MLIR.Generators.Emitters.Common;
 using MLIR.ODS.Model;
