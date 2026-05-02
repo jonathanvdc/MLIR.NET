fa9ce399d84e93bcd73cd21c20a3b624bae399f5
jonathanvdc <jonathan.vdc@outlook.com>
2026-05-02 13:10:32 -0400

    Refactor AppendBodySyntaxFields method to improve modularity and readability by moving logic to AssemblyFormatLowerer

--- src/MLIR.Generators/Emitters/AssemblyFormat/AssemblyFormatLowering.cs
+++ src/MLIR.Generators/Emitters/AssemblyFormat/AssemblyFormatLowering.cs
@@ -131,7 +131,7 @@ internal static class AssemblyFormatLowerer
         for (var i = 0; i < elements.Count; i++)
         {
             var start = metadata.Fields.Count;
-            EmitterHelpers.AppendBodySyntaxFields(usedNames, elements[i], operation, metadata);
+            AppendBodySyntaxFields(usedNames, elements[i], operation, metadata);
             var count = metadata.Fields.Count - start;
             var kind = GetOperationElementKind(elements[i]);
             loweredElements.Add(new LoweredOperationElement(
@@ -163,6 +163,406 @@ internal static class AssemblyFormatLowerer
             OptionalGroup _ => OperationFormatElementKind.OptionalGroup,
             OilistDirectiveChunk _ => OperationFormatElementKind.Oilist,
             _ => OperationFormatElementKind.Unsupported,
+        };
+    }
+
+    private static void AppendBodySyntaxFields(HashSet<string> usedNames, Element element, OperationModel operation, OperationBodySyntaxMetadata metadata, bool nullable = false)
+    {
+        switch (element)
+        {
+            case LiteralChunk literal:
+                foreach (var lit in literal.Value)
+                {
+                    switch (lit)
+                    {
+                        case PunctuationLiteral punc:
+                            AppendPunctuationField(usedNames, punc.TokenKind, metadata, nullable);
+                            break;
+
+                        case KeywordLiteral kw:
+                            AppendKeywordField(usedNames, kw.Spelling, metadata, nullable);
+                            break;
+                    }
+                }
+
+                break;
+
+            case VariableChunk variable:
+                AppendVariableField(usedNames, variable.Name, operation, metadata, nullable);
+                break;
+
+            case AttrDictDirectiveChunk _:
+                AppendDelimitedField(usedNames, metadata, "AttrDict", BodyComponentKind.AttrDict);
+                break;
+
+            case AttrDictWithKeywordDirectiveChunk _:
+                AppendDelimitedField(usedNames, metadata, "AttrDictWithKeyword", BodyComponentKind.AttrDictWithKeyword);
+                break;
+
+            case PropDictDirectiveChunk _:
+                AppendDelimitedField(usedNames, metadata, "PropDict", BodyComponentKind.PropDict);
+                break;
+
+            case RegionsDirectiveChunk _:
+            {
+                var name = EmitterHelpers.MakeUnique("Regions", usedNames);
+                var field = new BodySyntaxField(name, "IReadOnlyList<RegionSyntax>",
+                    "foreach (var region in " + name + ")\n" +
+                    "{\n" +
+                    "    writer.WriteRegion(region);\n" +
+                    "}");
+                metadata.AddField(field);
+                metadata.AddComponentField(new BodyComponentField(BodyComponentKind.Regions, "Regions", field.Name));
+                break;
+            }
+
+            case TypeDirectiveChunk typeDir:
+                AppendTypeField(
+                    usedNames,
+                    GetTypeBaseName(typeDir.Operand),
+                    EmitterHelpers.GetDirectiveOperandName(typeDir.Operand),
+                    operation,
+                    metadata,
+                    nullable,
+                    BodyComponentKind.TypeDirective);
+                break;
+
+            case SuccessorsDirectiveChunk _:
+                AppendDelimitedField(usedNames, metadata, "Successors", BodyComponentKind.Successors, "DelimitedSyntaxList<Token>");
+                break;
+
+            case OperandsDirectiveChunk _:
+                AppendDelimitedField(usedNames, metadata, "Operands", BodyComponentKind.Operands, "DelimitedSyntaxList<Token>");
+                break;
+
+            case QualifiedDirectiveChunk qualified:
+                AppendTypeField(
+                    usedNames,
+                    GetQualifiedTypeBaseName(qualified.Operand),
+                    EmitterHelpers.GetDirectiveOperandName(qualified.Operand),
+                    operation,
+                    metadata,
+                    nullable,
+                    BodyComponentKind.TypeDirective);
+                break;
+
+            case ResultsDirectiveChunk _:
+                AppendTypeField(
+                    usedNames,
+                    "ResultsType",
+                    "Results",
+                    operation,
+                    metadata,
+                    nullable,
+                    BodyComponentKind.ResultsDirective);
+                break;
+
+            case FunctionalTypeDirectiveChunk _:
+                AppendTypeField(
+                    usedNames,
+                    "FunctionalType",
+                    "Type",
+                    operation,
+                    metadata,
+                    nullable,
+                    BodyComponentKind.FunctionalTypeDirective);
+                break;
+
+            case OptionalGroup optionalGroup:
+                foreach (var inner in optionalGroup.ThenElements)
+                {
+                    AppendBodySyntaxFields(usedNames, inner, operation, metadata, nullable: true);
+                }
+
+                if (optionalGroup.ElseElements != null)
+                {
+                    foreach (var inner in optionalGroup.ElseElements)
+                    {
+                        AppendBodySyntaxFields(usedNames, inner, operation, metadata, nullable: true);
+                    }
+                }
+
+                break;
+
+            case OilistDirectiveChunk oilist:
+                foreach (var clause in oilist.Clauses)
+                {
+                    AppendKeywordField(usedNames, clause.Keyword, metadata, nullable: true, isOilistKeyword: true);
+
+                    foreach (var oiElem in clause.Elements)
+                    {
+                        AppendOilistElementFields(usedNames, oiElem, operation, metadata);
+                    }
+                }
+
+                break;
+        }
+    }
+
+    private static void AppendDelimitedField(
+        HashSet<string> usedNames,
+        OperationBodySyntaxMetadata metadata,
+        string baseName,
+        BodyComponentKind componentKind,
+        string csType = "DelimitedSyntaxList<NamedAttributeSyntax>")
+    {
+        var name = EmitterHelpers.MakeUnique(baseName, usedNames);
+        var field = new BodySyntaxField(name, csType, "writer.WriteDelimitedList(" + name + ", \" \");");
+        metadata.AddField(field);
+        metadata.AddComponentField(new BodyComponentField(componentKind, baseName, field.Name));
+    }
+
+    private static void AppendOilistElementFields(HashSet<string> usedNames, OilistElement element, OperationModel operation, OperationBodySyntaxMetadata metadata)
+    {
+        switch (element)
+        {
+            case OilistVariableElement variable:
+                AppendVariableField(usedNames, variable.Name, operation, metadata, nullable: true);
+                break;
+
+            case OilistTypeDirectiveElement typeDir:
+                AppendTypeField(
+                    usedNames,
+                    GetTypeBaseName(typeDir.Operand),
+                    EmitterHelpers.GetDirectiveOperandName(typeDir.Operand),
+                    operation,
+                    metadata,
+                    nullable: true,
+                    BodyComponentKind.TypeDirective);
+                break;
+
+            case OilistLiteralElement literal:
+            {
+                var name = EmitterHelpers.MakeUnique(DialectGeneratorNaming.ToPascalCase(literal.Value) + "Token", usedNames);
+                var field = new BodySyntaxField(name, "Token?",
+                    "if (" + name + ".HasValue) writer.WriteToken(" + name + ".Value);");
+                metadata.AddField(field);
+                metadata.AddComponentField(new BodyComponentField(BodyComponentKind.Literal, "OilistLiteral:" + literal.Value, field.Name));
+                break;
+            }
+        }
+    }
+
+    private static void AppendPunctuationField(HashSet<string> usedNames, TokenKind tokenKind, OperationBodySyntaxMetadata metadata, bool nullable)
+    {
+        var name = EmitterHelpers.MakeUnique(GetPunctuationFieldName(tokenKind), usedNames);
+        var leadingTrivia = GetPunctuationLeadingTrivia(tokenKind);
+        var (csType, writeStmt) = nullable
+            ? ("Token?", "if (" + name + ".HasValue) writer.WriteToken(" + name + ".Value, \"" + leadingTrivia + "\");")
+            : ("Token", "writer.WriteToken(" + name + ", \"" + leadingTrivia + "\");");
+        var field = new BodySyntaxField(name, csType, writeStmt);
+        metadata.AddField(field);
+        metadata.AddComponentField(new BodyComponentField(BodyComponentKind.Literal, "Punctuation:" + tokenKind, field.Name));
+    }
+
+    private static void AppendKeywordField(HashSet<string> usedNames, string spelling, OperationBodySyntaxMetadata metadata, bool nullable, bool isOilistKeyword = false)
+    {
+        var name = EmitterHelpers.MakeUnique(DialectGeneratorNaming.ToPascalCase(spelling) + "Keyword", usedNames);
+        var leadingTrivia = isOilistKeyword ? "\\n    " : " ";
+        var (csType, writeStmt) = nullable
+            ? ("Token?", "if (" + name + ".HasValue) writer.WriteToken(" + name + ".Value, \"" + leadingTrivia + "\");")
+            : ("Token", "writer.WriteToken(" + name + ", \"" + leadingTrivia + "\");");
+        var field = new BodySyntaxField(name, csType, writeStmt);
+        metadata.AddField(field);
+        metadata.AddComponentField(new BodyComponentField(BodyComponentKind.Literal, "Keyword:" + spelling, field.Name));
+    }
+
+    private static void AppendVariableField(HashSet<string> usedNames, string variableName, OperationModel operation, OperationBodySyntaxMetadata metadata, bool nullable)
+    {
+        var name = EmitterHelpers.MakeUnique(DialectGeneratorNaming.ToPascalCase(variableName), usedNames);
+        if (EmitterHelpers.ContainsName(operation.Attributes, variableName, static attribute => attribute.Name))
+        {
+            var (csType, writeStmt) = nullable
+                ? ("AttributeValueSyntax?", "if (" + name + " != null) { writer.SuggestTrivia(\" \"); " + name + ".WriteTo(writer); }")
+                : ("AttributeValueSyntax", "writer.SuggestTrivia(\" \"); " + name + ".WriteTo(writer);");
+            var field = new BodySyntaxField(name, csType, writeStmt);
+            metadata.AddField(field);
+            metadata.AddComponentField(new BodyComponentField(BodyComponentKind.Attribute, variableName, field.Name));
+        }
+        else if (EmitterHelpers.ContainsName(operation.Regions, variableName, static region => region.Name))
+        {
+            var isVariadic = IsVariadicRegion(operation, variableName);
+            string csType;
+            string writeStmt;
+            if (isVariadic)
+            {
+                csType = "global::System.Collections.Generic.IReadOnlyList<RegionSyntax>";
+                writeStmt = "foreach (var region in " + name + ") { writer.WriteRegion(region); }";
+            }
+            else
+            {
+                csType = nullable ? "RegionSyntax?" : "RegionSyntax";
+                writeStmt = nullable
+                    ? "if (" + name + ".HasValue) writer.WriteRegion(" + name + ".Value);"
+                    : "writer.WriteRegion(" + name + ");";
+            }
+
+            var field = new BodySyntaxField(name, csType, writeStmt);
+            metadata.AddField(field);
+            metadata.AddComponentField(new BodyComponentField(
+                EmitterHelpers.GetComponentKindForVariable(operation, variableName),
+                variableName,
+                field.Name));
+        }
+        else if (IsVariadicOperand(operation, variableName))
+        {
+            const string csType = "global::System.Collections.Generic.IReadOnlyList<Token>";
+            var writeStmt =
+                "for (var _i = 0; _i < " + name + ".Count; _i++) { " +
+                "if (_i > 0) writer.WriteToken(TokenFactory.Comma(), \"\"); " +
+                "writer.WriteToken(" + name + "[_i], \" \"); }";
+            var field = new BodySyntaxField(name, csType, writeStmt);
+            metadata.AddField(field);
+            metadata.AddComponentField(new BodyComponentField(
+                EmitterHelpers.GetComponentKindForVariable(operation, variableName),
+                variableName,
+                field.Name));
+        }
+        else
+        {
+            var (csType, writeStmt) = nullable
+                ? ("Token?", "if (" + name + ".HasValue) writer.WriteToken(" + name + ".Value, \" \");")
+                : ("Token", "writer.WriteToken(" + name + ", \" \");");
+            var field = new BodySyntaxField(name, csType, writeStmt);
+            metadata.AddField(field);
+            metadata.AddComponentField(new BodyComponentField(
+                EmitterHelpers.GetComponentKindForVariable(operation, variableName),
+                variableName,
+                field.Name));
+        }
+    }
+
+    private static bool IsVariadicOperand(OperationModel operation, string variableName)
+    {
+        foreach (var operand in operation.Operands)
+        {
+            if (string.Equals(operand.Name, variableName, System.StringComparison.Ordinal))
+            {
+                return operand.IsVariadic;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsVariadicResult(OperationModel operation, string variableName)
+    {
+        foreach (var result in operation.Results)
+        {
+            if (string.Equals(result.Name, variableName, System.StringComparison.Ordinal))
+            {
+                return result.IsVariadic;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsVariadicRegion(OperationModel operation, string variableName)
+    {
+        foreach (var region in operation.Regions)
+        {
+            if (string.Equals(region.Name, variableName, System.StringComparison.Ordinal))
+            {
+                return region.IsVariadic;
+            }
+        }
+
+        return false;
+    }
+
+    private static void AppendTypeField(
+        HashSet<string> usedNames,
+        string baseName,
+        string operandName,
+        OperationModel operation,
+        OperationBodySyntaxMetadata metadata,
+        bool nullable,
+        BodyComponentKind componentKind)
+    {
+        var name = EmitterHelpers.MakeUnique(baseName, usedNames);
+        string csType;
+        string writeStmt;
+        if (IsVariadicOperand(operation, operandName) || IsVariadicResult(operation, operandName))
+        {
+            csType = "IReadOnlyList<TypeSyntax>";
+            writeStmt =
+                "for (var i = 0; i < " + name + ".Count; i++)\n" +
+                "{\n" +
+                "    if (i > 0)\n" +
+                "    {\n" +
+                "        writer.WriteToken(TokenFactory.Comma());\n" +
+                "    }\n" +
+                "\n" +
+                "    writer.SuggestTrivia(\" \");\n" +
+                "    " + name + "[i].WriteTo(writer);\n" +
+                "}";
+        }
+        else
+        {
+            (csType, writeStmt) = nullable
+                ? ("TypeSyntax?", "if (" + name + " != null) { writer.SuggestTrivia(\" \"); " + name + ".WriteTo(writer); }")
+                : ("TypeSyntax", "writer.SuggestTrivia(\" \"); " + name + ".WriteTo(writer);");
+        }
+
+        var field = new BodySyntaxField(name, csType, writeStmt);
+        metadata.AddField(field);
+        metadata.AddComponentField(new BodyComponentField(componentKind, operandName, field.Name));
+    }
+
+    private static string GetTypeBaseName(DirectiveOperand operand)
+    {
+        return operand is VariableOperand varOp
+            ? DialectGeneratorNaming.ToPascalCase(varOp.Name) + "Type"
+            : "Type";
+    }
+
+    private static string GetQualifiedTypeBaseName(DirectiveOperand operand)
+    {
+        if (operand is TypeDirectiveOperand tdo && tdo.Operand is VariableOperand tVar)
+        {
+            return DialectGeneratorNaming.ToPascalCase(tVar.Name) + "Type";
+        }
+
+        return GetTypeBaseName(operand);
+    }
+
+    private static string GetPunctuationLeadingTrivia(TokenKind tokenKind)
+    {
+        return tokenKind switch
+        {
+            TokenKind.Colon => " ",
+            TokenKind.Arrow => " ",
+            TokenKind.Equal => " ",
+            _ => string.Empty,
+        };
+    }
+
+    private static string GetPunctuationFieldName(TokenKind tokenKind)
+    {
+        return tokenKind switch
+        {
+            TokenKind.Comma => "CommaToken",
+            TokenKind.LParen => "LParenToken",
+            TokenKind.RParen => "RParenToken",
+            TokenKind.LBracket => "LBracketToken",
+            TokenKind.RBracket => "RBracketToken",
+            TokenKind.LBrace => "LBraceToken",
+            TokenKind.RBrace => "RBraceToken",
+            TokenKind.Arrow => "ArrowToken",
+            TokenKind.Colon => "ColonToken",
+            TokenKind.Equal => "EqualToken",
+            TokenKind.LessThan => "LessThanToken",
+            TokenKind.GreaterThan => "GreaterThanToken",
+            TokenKind.Question => "QuestionToken",
+            TokenKind.Star => "StarToken",
+            TokenKind.Plus => "PlusToken",
+            TokenKind.Minus => "MinusToken",
+            TokenKind.Dot => "DotToken",
+            TokenKind.At => "AtToken",
+            TokenKind.Hash => "HashToken",
+            TokenKind.SymbolName => "SymbolNameToken",
+            _ => "Token",
         };
     }
 
